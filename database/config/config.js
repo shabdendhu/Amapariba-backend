d9d549a209194029c837280c2f545287f678b705
shabdendhu <tshabdendhu@gmail.com>
2021-06-21 00:11:31 +0530

    basic devlopment

--- database/config/config.js
+++ database/config/config.js
@@ -2,31 +2,33 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    // "username": "admin",
-    // "password": "hD9ghw9e",
-    // "database": "database_2",
-    // "host": "mysql-34618-0.cloudclusters.net",
-    // "dialect": "mysql",
-    // "port":"34618"
+    "username": "admin",
+    "password": "hD9ghw9e",
+    "database": "database_2",
+    "host": "mysql-34618-0.cloudclusters.net",
+    "dialect": "mysql",
+    "port":"34618"
 
-    username: "kishan",
-    password: "manage",
-    database: "GraphQl_Nodejs_Sequlize",
-    host: "localhost",
-    dialect: "mysql",
+    // username: "kishan",
+    // password: "manage",
+    // database: "GraphQl_Nodejs_Sequlize",
+    // host: "localhost",
+    // dialect: "mysql",
   },
-  // "test": {
-  //   "username": "kishan",
-  //   "password": "manage",
-  //   "database": "GraphQl_Nodejs_Sequlize",
-  //   "host": "localhost",
-  //   "dialect": "mysql"
-  // },
-  // "production": {
-  //   "username": "kishan",
-  //   "password": "manage",
-  //   "database": "GraphQl_Nodejs_Sequlize",
-  //   "host": "localhost",
-  //   "dialect": "mysql"
-  // }
+  "test": {
+    "username": "admin",
+    "password": "hD9ghw9e",
+    "database": "database_2",
+    "host": "mysql-34618-0.cloudclusters.net",
+    "dialect": "mysql",
+    "port":"34618"
+  },
+  "production": {
+    "username": "admin",
+    "password": "hD9ghw9e",
+    "database": "database_2",
+    "host": "mysql-34618-0.cloudclusters.net",
+    "dialect": "mysql",
+    "port":"34618"
+  }
 };
