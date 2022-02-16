edabd2bfa237e4930d4de8097705eb20635924f9
shabdendhu <tshabdendhu@gmail.com>
2022-02-16 22:26:43 +0530

    besic devlopment

--- database/config/config.js
+++ database/config/config.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "kishan",
-    database: "database_2",
-    host: "localhost",
+    // username: "root",
+    // password: "kishan",
+    // database: "database_2",
+    // host: "localhost",
+    // dialect: "mysql",
+    // port: "3306",
+    username: 'admin',
+    password: 'k2P6YSEM',
+    database: 'database_2',
+    host: 'mysql-69241-0.cloudclusters.net',
     dialect: "mysql",
-    port: "3306",
+    port:'19026',
   },
   test: {
     username: process.env.DBUSERNAME,
@@ -18,11 +24,11 @@ module.exports = {
     port: process.env.DBPORT,
   },
   production: {
-    username: process.env.DBUSERNAME,
-    password: process.env.DBPASSWORD,
-    database: process.env.DBDATABASE,
-    host: process.env.DBHOST,
+    username: 'admin',
+    password: 'k2P6YSEM',
+    database: 'database_2',
+    host: 'mysql-69241-0.cloudclusters.net',
     dialect: "mysql",
-    port: process.env.DBPORT,
+    port:'19026',
   },
 };
